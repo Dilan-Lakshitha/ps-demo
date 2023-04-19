1715b76df104a45c06f10ff1b9f8a46d91e1ae1d
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-19 14:30:18 +0530

    data model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { UserSettingsFormComponent } from './user-settings-form/user-settings-fo
     UserSettingsFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
