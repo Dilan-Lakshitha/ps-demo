5d6feefb3e3e690076dd4df4149b8f4051ab0ea6
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-19 16:01:36 +0530

    newly bootstrap add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,9 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
